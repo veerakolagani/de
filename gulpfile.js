1051ad0abf11e261247193694663f90810be10a0
veerakolagani <kolaganies@gmail.com>
2018-02-01 13:01:55 +0530

    name

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,11 @@ var check = require('gulp-check');
 var util = require('gulp-util');
  
 gulp.task('check', function () {
-  gulp.src(['test/**/*.js'])
+  gulp.src(['index.php'])
     .pipe(check(/TODO/))
     .on('error', function (err) {
       util.beep();
       util.log(util.colors.red(err));
     });
 });
+module.exports = gulp;
